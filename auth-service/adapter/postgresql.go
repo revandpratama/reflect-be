4b1d5b5c42d5e085eb23f8395edad4e91e534a9e
revandpratama <revandaustin@gmail.com>
2025-02-28 11:31:44 +0700

    chore: delete debugging print

--- auth-service/adapter/postgresql.go
+++ auth-service/adapter/postgresql.go
@@ -16,8 +16,6 @@ func (p *PostgresOption) Start(a *Adapter) error {
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
 		config.ENV.DBHost, config.ENV.DBUser, config.ENV.DBPassword, config.ENV.DBName, config.ENV.DBPort, config.ENV.DBSSLMode)
 
-	fmt.Println("Generated DSN:", dsn)
-
 	db, err := gorm.Open(postgres.New(postgres.Config{
 		DSN:                  dsn,
 		PreferSimpleProtocol: true,
