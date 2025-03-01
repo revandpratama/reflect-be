70084133f15d03e7ae09cc7651df6c1e43dd3f7a
revandpratama <revandaustin@gmail.com>
2025-03-02 00:12:30 +0700

    feat: implement kafka to log

--- auth-service/helper/logger.go
+++ auth-service/helper/logger.go
@@ -1,10 +1,12 @@
 package helper
 
 import (
+	"context"
 	"fmt"
 	"time"
 
 	"github.com/rs/zerolog/log"
+	"github.com/segmentio/kafka-go"
 )
 
 type Log struct {
@@ -17,7 +19,7 @@ type Log struct {
 func NewLog() *Log {
 	return &Log{
 		Timestamp: time.Now(),
-		Source: "auth-service",
+		Source:    "auth-service",
 	}
 }
 
@@ -43,6 +45,27 @@ func (l *Log) Error(msg string) *Log {
 	return l
 }
 
+var KafkaWriter *kafka.Writer
+
 func (l *Log) ToKafka() {
-	fmt.Println("produced to kafka")
+
+	err := KafkaWriter.WriteMessages(context.Background(),
+		kafka.Message{
+			Key:   []byte("Key-A"),
+			Value: []byte("Hello World!"),
+		},
+		kafka.Message{
+			Key:   []byte("Key-B"),
+			Value: []byte("One!"),
+		},
+		kafka.Message{
+			Key:   []byte("Key-C"),
+			Value: []byte("Two!"),
+		},
+	)
+	if err != nil {
+		log.Error().Msg(fmt.Sprintf("failed to write messages: %v ", err))
+	}
+
+	fmt.Println("wrote messages")
 }
