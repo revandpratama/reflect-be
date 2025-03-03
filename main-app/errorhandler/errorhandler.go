e6545919d0f6e1642b2f89bfd2abaec1b8f20206
revandpratama <revandaustin@gmail.com>
2025-03-03 19:29:30 +0700

    feat: create error builder for http error

---
+++ main-app/errorhandler/errorhandler.go
@@ -0,0 +1,31 @@
+package errorhandler
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/revandpratama/reflect/helper/response"
+	"github.com/revandpratama/reflect/types"
+)
+
+func BuildError(c *fiber.Ctx, err error) error {
+	var statusCode int
+
+	switch err.(type) {
+	case *types.NotFoundError:
+		statusCode = fiber.StatusNotFound
+	case *types.BadRequestError:
+		statusCode = fiber.StatusBadRequest
+	case *types.UnauthorizedError:
+		statusCode = fiber.StatusUnauthorized
+	case *types.InternalServerError:
+		statusCode = fiber.StatusInternalServerError
+	default:
+		statusCode = fiber.StatusInternalServerError
+	}
+
+	response := response.NewResponse(&types.ResponseParams{
+		StatusCode: statusCode,
+		Message:    err.Error(),
+	})
+
+	return c.JSON(response)
+}
