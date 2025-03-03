f72e591b70f0e93a8f3ce2224e25aca458e14453
revandpratama <revandaustin@gmail.com>
2025-03-04 04:35:00 +0700

    feat: add post routes

---
+++ main-app/internal/routes/post_routes.go
@@ -0,0 +1,25 @@
+package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/revandpratama/reflect/internal/handlers"
+	"github.com/revandpratama/reflect/internal/repositories"
+	"github.com/revandpratama/reflect/internal/services"
+	"gorm.io/gorm"
+)
+
+func InitPostHandler(db *gorm.DB) handlers.PostHandler {
+	repo := repositories.NewPostRepository(db)
+	service := services.NewPostService(repo)
+	return handlers.NewPostHandler(service)
+}
+
+func InitPostRoutes(r fiber.Router, handler handlers.PostHandler) {
+	// r.Get("/posts", handler.GetAllPosts)
+	// r.Get("/posts/:id", handler.GetPostByID)
+	// r.Post("/posts", handler.CreatePost)
+	// r.Put("/posts/:id", handler.UpdatePost)
+	// r.Delete("/posts/:id", handler.DeletePost)
+}
+
+
