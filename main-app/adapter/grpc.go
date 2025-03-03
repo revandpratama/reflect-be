5148131675f06d70fed5c9e8f4941efd20800e42
revandpratama <revandaustin@gmail.com>
2025-03-03 22:47:06 +0700

    refactor: use config env for grpc port

--- main-app/adapter/grpc.go
+++ main-app/adapter/grpc.go
@@ -3,6 +3,7 @@ package adapter
 import (
 	"fmt"
 
+	"github.com/revandpratama/reflect/config"
 	"github.com/revandpratama/reflect/helper"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -16,7 +17,7 @@ func (g *GRPCOption) Start(a *Adapter) error {
 
 	helper.NewLog().Info("initializing grpc server...").ToKafka()
 
-	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.NewClient(fmt.Sprintf(":%v", config.ENV.GRPCServerPort), grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		return fmt.Errorf("failed to connect to server: %v", err)
 	}
