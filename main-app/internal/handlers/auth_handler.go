85b1a1c98c5ce303943a9df3b55124510c2fc710
revandpratama <revandaustin@gmail.com>
2025-03-03 22:28:26 +0700

    feat(auth): create auth handler that utilize grpc from auth microservie

---
+++ main-app/internal/handlers/auth_handler.go
@@ -0,0 +1,67 @@
+package handlers
+
+import (
+	"context"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/revandpratama/reflect/errorhandler"
+	"github.com/revandpratama/reflect/helper/response"
+	"github.com/revandpratama/reflect/internal/dto"
+	pb "github.com/revandpratama/reflect/internal/generatedProtobuf/auth"
+	"github.com/revandpratama/reflect/types"
+	"google.golang.org/grpc"
+)
+
+type authHandler struct {
+	ctx    context.Context
+	client pb.AuthServiceClient
+}
+
+type AuthHandler interface {
+	Login(c *fiber.Ctx) error
+}
+
+func NewAuthHandler(conn *grpc.ClientConn) AuthHandler {
+	return &authHandler{
+		ctx:    context.Background(),
+		client: pb.NewAuthServiceClient(conn),
+	}
+}
+
+func (h *authHandler) Login(c *fiber.Ctx) error {
+	var req dto.LoginRequest
+	if err := c.BodyParser(&req); err != nil {
+		return errorhandler.BuildError(c, &types.BadRequestError{Message: err.Error()})
+	}
+
+	res, err := h.client.Login(h.ctx, &pb.LoginRequest{Email: req.Email, Password: req.Password})
+	if err != nil {
+		return errorhandler.BuildError(c, &types.InternalServerError{Message: err.Error()})
+	}
+
+	response := response.NewResponse(&types.ResponseParams{
+		StatusCode: fiber.StatusOK,
+		Message:    "login success",
+		Data:       map[string]string{"token": res.AccessToken},
+	})
+	c.JSON(response)
+
+	return nil
+}
+
+func (h *authHandler) Register(c *fiber.Ctx) error {
+	var req dto.RegisterRequest
+	if err := c.BodyParser(&req); err != nil {
+		return errorhandler.BuildError(c, &types.BadRequestError{Message: err.Error()})
+	}
+
+	res, err := h.client.Register(h.ctx, &pb.RegisterRequest{Email: req.Email, Password: req.Password})
+	if err != nil {
+		return errorhandler.BuildError(c, &types.InternalServerError{Message: err.Error()})
+	}
+	response := response.NewResponse(&types.ResponseParams{
+		StatusCode: fiber.StatusOK,
+		Message:    res.Message,
+	})
+	return c.JSON(response)
+}
