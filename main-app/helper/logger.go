a529cb5beacd2b98f6fe8b9825866ad99ee6ba5c
revandpratama <revandaustin@gmail.com>
2025-03-03 07:23:29 +0700

    feat: add helper for logging feature

---
+++ main-app/helper/logger.go
@@ -0,0 +1,64 @@
+package helper
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/rs/zerolog/log"
+	"github.com/segmentio/kafka-go"
+)
+
+type Log struct {
+	Level     string
+	Msg       string
+	Source    string
+	Timestamp time.Time
+}
+
+func NewLog() *Log {
+	return &Log{
+		Timestamp: time.Now(),
+		Source:    "auth-service",
+	}
+}
+
+func (l *Log) Info(msg string) *Log {
+	l.Level = "INFO"
+	l.Msg = msg
+
+	log.Info().Msg(msg)
+	return l
+}
+func (l *Log) Fatal(msg string) *Log {
+	l.Level = "FATAL"
+	l.Msg = msg
+
+	log.Fatal().Msg(msg)
+	return l
+}
+func (l *Log) Error(msg string) *Log {
+	l.Level = "ERROR"
+	l.Msg = msg
+
+	log.Error().Msg(msg)
+	return l
+}
+
+var KafkaWriter *kafka.Writer
+
+func (l *Log) ToKafka() {
+
+	err := KafkaWriter.WriteMessages(context.Background(),
+		kafka.Message{
+			Key:   []byte(l.Source),
+			Value: []byte(l.Msg),
+			Time:  l.Timestamp,
+		},
+	)
+	if err != nil {
+		log.Error().Msg(fmt.Sprintf("failed to write messages: %v ", err))
+	}
+
+	fmt.Println("wrote messages")
+}
