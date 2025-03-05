662fa524643dc56b634ffcf93820af425f3a5526
revandpratama <revandaustin@gmail.com>
2025-03-05 16:17:21 +0700

    feat: add update and delete image mechanism

--- main-app/internal/services/post_service.go
+++ main-app/internal/services/post_service.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"context"
+	"mime/multipart"
 
 	"github.com/minio/minio-go/v7"
 	"github.com/revandpratama/reflect/helper"
@@ -13,7 +14,7 @@ import (
 )
 
 type postService struct {
-	repo repositories.PostRepository
+	repo        repositories.PostRepository
 	minioClient *minio.Client
 }
 
@@ -22,28 +23,27 @@ type PostService interface {
 	GetPostByID(ctx context.Context, id int) (*dto.PostResponse, error)
 	GetPostByUserID(ctx context.Context, userID int) ([]dto.PostResponse, error)
 	CreatePost(ctx context.Context, req *dto.PostRequest) error
-	UpdatePost(ctx context.Context, req *dto.PostRequest) error
+	UpdatePost(ctx context.Context, id int, req *dto.PostRequest) error
 	DeletePost(ctx context.Context, id int) error
 }
 
 func NewPostService(repo repositories.PostRepository, minioCLient *minio.Client) PostService {
 	return &postService{
-		repo: repo,
+		repo:        repo,
 		minioClient: minioCLient,
 	}
 }
 
 func (p *postService) CreatePost(ctx context.Context, req *dto.PostRequest) error {
 
-	// TODO: implement image upload
 	post := entities.Post{
-		UserID:   req.UserID,
-		Title:    req.Title,
-		Body:     req.Body,
+		UserID: req.UserID,
+		Title:  req.Title,
+		Body:   req.Body,
 	}
 
 	if req.Image != nil {
-		imgUrl, err := helper.UploadObject(ctx, p.minioClient, "posts", req.Image)
+		imgUrl, err := helper.UploadObject(ctx, p.minioClient, helper.MINIO_POST_BUCKET, req.Image)
 		if err != nil {
 			return &types.InternalServerError{Message: err.Error()}
 		}
@@ -103,14 +103,23 @@ func (p *postService) GetPostByUserID(ctx context.Context, userID int) ([]dto.Po
 	return postResponses, nil
 }
 
-func (p *postService) UpdatePost(ctx context.Context, req *dto.PostRequest) error {
+func (p *postService) UpdatePost(ctx context.Context, id int, req *dto.PostRequest) error {
 	post := &entities.Post{
 		UserID: req.UserID,
 		Title:  req.Title,
 		Body:   req.Body,
-		// TODO : add image upload
 	}
-	if err := p.repo.UpdatePost(ctx, post); err != nil {
+
+	if req.Image != nil {
+		newUrl, err := p.updateImageUrl(ctx, p.minioClient, id, helper.MINIO_POST_BUCKET, req.Image)
+		if err != nil {
+			return &types.InternalServerError{Message: err.Error()}
+		}
+
+		post.ImageUrl = &newUrl
+	}
+
+	if err := p.repo.UpdatePost(ctx, id, post); err != nil {
 		if err == gorm.ErrRecordNotFound {
 			return &types.NotFoundError{Message: err.Error()}
 		}
@@ -121,11 +130,53 @@ func (p *postService) UpdatePost(ctx context.Context, req *dto.PostRequest) erro
 }
 
 func (p *postService) DeletePost(ctx context.Context, id int) error {
+
+	if err := p.deleteImageUrl(ctx, p.minioClient, id, helper.MINIO_POST_BUCKET); err != nil {
+		return &types.InternalServerError{Message: err.Error()}
+	}
+
 	if err := p.repo.DeletePost(ctx, id); err != nil {
 		if err == gorm.ErrRecordNotFound {
 			return &types.NotFoundError{Message: err.Error()}
 		}
 		return &types.InternalServerError{Message: err.Error()}
+	}
+
+	return nil
+}
+
+func (s *postService) updateImageUrl(ctx context.Context, minioClient *minio.Client, postID int, bucketName string, image *multipart.FileHeader) (string, error) {
+	post, err := s.repo.GetPostByID(ctx, postID)
+	if err != nil {
+		return "", err
+	}
+
+	newUrl, err := helper.UploadObject(ctx, minioClient, bucketName, image)
+	if err != nil {
+		return "", err
+	}
+
+	if post.ImageUrl != nil {
+		err = helper.DeleteObject(ctx, minioClient, bucketName, *post.ImageUrl)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	return newUrl, nil
+}
+
+func (s *postService) deleteImageUrl(ctx context.Context, minioClient *minio.Client, postID int, bucketName string) error {
+	post, err := s.repo.GetPostByID(ctx, postID)
+	if err != nil {
+		return err
+	}
+
+	if post.ImageUrl != nil {
+		err = helper.DeleteObject(ctx, minioClient, bucketName, *post.ImageUrl)
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
