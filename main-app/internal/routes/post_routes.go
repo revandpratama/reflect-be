2c1aca08278e60a428069096a61873542ec9d49f
revandpratama <revandaustin@gmail.com>
2025-03-06 14:07:44 +0700

    feat: group post route and use auth middleware

--- main-app/internal/routes/post_routes.go
+++ main-app/internal/routes/post_routes.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/minio/minio-go/v7"
 	"github.com/revandpratama/reflect/internal/handlers"
+	"github.com/revandpratama/reflect/internal/middleware"
 	"github.com/revandpratama/reflect/internal/repositories"
 	"github.com/revandpratama/reflect/internal/services"
 	"gorm.io/gorm"
@@ -16,11 +17,12 @@ func InitPostHandler(db *gorm.DB, minioClient *minio.Client) handlers.PostHandle
 }
 
 func InitPostRoutes(r fiber.Router, handler handlers.PostHandler) {
+	post := r.Group("/posts")
+	post.Use(middleware.AuthMiddleware())
+
 	r.Get("/posts", handler.GetAllPosts)
 	r.Get("/posts/:id", handler.GetPostByID)
 	r.Post("/posts", handler.CreatePost)
 	r.Put("/posts/:id", handler.UpdatePost)
 	r.Delete("/posts/:id", handler.DeletePost)
 }
-
-
