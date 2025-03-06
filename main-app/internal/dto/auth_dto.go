862495ff01611a83fe26190e326f832afa2daad1
revandpratama <revandaustin@gmail.com>
2025-03-06 14:29:46 +0700

    feat: add more validation requirement for login request

--- main-app/internal/dto/auth_dto.go
+++ main-app/internal/dto/auth_dto.go
@@ -1,8 +1,8 @@
 package dto
 
 type LoginRequest struct {
-	Email    string `json:"email" validate:"required"`
-	Password string `json:"password" validate:"required"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=8,max=128"`
 }
 
 type LoginResponse struct {
